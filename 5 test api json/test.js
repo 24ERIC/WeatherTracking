8e350ac103e8e11ac1197046c41241cb761b01f5
24ERIC <60751655+24ERIC@users.noreply.github.com>
2023-02-09 09:46:14 -0500

    finish 5

--- 5 test api json/test.js
+++ 5 test api json/test.js
@@ -1,37 +1,8 @@
-async function populate() {
-
+(async function populate() {
     const requestURL = 'https://api.weather.gov/gridpoints/OKX/33,36/forecast';
     const request = new Request(requestURL);
-  
     const response = await fetch(request);
-
-
     const superHeroesText = await response.text();
-    // for (let i = 0; i < JSON.parse(superHeroesText).properties.periods.length; i+=2) {
-    //     const period = JSON.parse(superHeroesText).properties.periods[i];
-    //     const name = period.name;
-    //     const temperature = period.temperature;
-    //     const windSpeed = period.windSpeed;
-    //     const windDirection = period.windDirection;
-    //     const shortForecast = period.shortForecast;
-    //     const detailedForecast = period.detailedForecast;
-    // } 
     const i = 0;
-    document.getElementById("content").innerHTML = JSON.stringify(
-        JSON.parse(superHeroesText).properties.periods[i].name 
-        + " has temperature " 
-        + JSON.parse(superHeroesText).properties.periods[i].temperature 
-        + " with wind speed of "
-        + JSON.parse(superHeroesText).properties.periods[i].windSpeed
-        + " at "
-        + JSON.parse(superHeroesText).properties.periods[i].windDirection
-        + " direction. In Near Future, weather will be "
-        + JSON.parse(superHeroesText).properties.periods[i].detailedForecast
-        + "."
-        );
-
-  }
-  
-
-
-populate()
+    document.getElementById("content").innerHTML = JSON.stringify(JSON.parse(superHeroesText).properties.periods[i].detailedForecast);
+    })()
